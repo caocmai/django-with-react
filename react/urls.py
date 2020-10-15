5a8f0a3570db0d76858abf2007e1e987ee6eb0a0
Cao Mai <caomai@rocketmail.com>
2020-10-15 08:56:24 -0700

    [update] Heroku

--- react/urls.py
+++ react/urls.py
@@ -20,5 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('react/', include('first_react.urls')),
+    # The index.html is from the reactapp folder that has the React app, 
+    # and not using Django templating
     path('', TemplateView.as_view(template_name='index.html'))
 ]
