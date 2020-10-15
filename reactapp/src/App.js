be506fa6857f79bb17eeea7cddbb4fb67e6a048f
Cao Mai <caomai@rocketmail.com>
2020-10-14 22:39:54 -0700

    Git track reactapp

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button  type="button" class="btn btn-outline-success">SUCESS!</button>
+      <h1> First React Project with Django</h1>
+      <img src={logo} alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
